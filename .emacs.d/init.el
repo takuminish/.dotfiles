fa65e68519c1d81bda66cab83e2ddd24231cac63
takuminv <takuminv@TakuAir.local>
2019-01-07 00:54:38 +0900

    string-faceの色を変更

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -26,3 +26,15 @@
 (define-key global-map(kbd "C-n")'forward-char)
 (define-key global-map(kbd "C-b")'backward-char)
 (define-key global-map(kbd "C-f")'next-line)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(font-lock-string-face ((t (:foreground "color-84")))))
