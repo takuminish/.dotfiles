c856b01f4667076f9a694bd2e246f00dd91f2433
takuminv <takuminv@TakuAir.local>
2019-01-08 00:18:08 +0900

    オプションの記述を分割

---
+++ .zsh.d/option.zsh
@@ -0,0 +1,34 @@
+# 色を使用できるようにする
+autoload -Uz colors
+colors
+
+# ヒストリ(履歴)の設定
+HISTFILE=~/.zsh_history
+HISTSIZE=100000
+SAVEHIST=100000
+
+# 補完機能を使用できるようにする
+autoload -Uz compinit
+compinit
+
+# ディレクトリ名だけでcdする
+setopt auto_cd
+
+# cdしたあと自動でlsする
+function chpwd() { ls }
+
+
+zle -N alls
+bindkey "\C-m" alls
+export PATH="$HOME/.rbenv/bin:$PATH"
+eval "$(rbenv init -)"
+
+compinit
+
+export LSCOLORS=exfxcxdxbxegedabagacad
+export LS_COLORS='di=34:ln=35:so=32:pi=33:ex=31:bd=46;34:cd=43;34:su=41;30:sg=46;30:tw=42;30:ow=43;30'
+
+zstyle ':completion:*' list-colors 'di=34' 'ln=35' 'so=32' 'ex=31' 'bd=46;34' 'cd=43;34'
+
+# 補完後、メニュー選択モードになり左右キーで移動が出来る
+zstyle ':completion:*:default' menu select=2
