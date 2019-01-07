df3fe4850a0fcbcb60978b123ab5f18362bca8fa
takuminv <takuminv@TakuAir.local>
2019-01-08 01:52:26 +0900

    補完の際に色を付ける

--- .zsh.d/option.zsh
+++ .zsh.d/option.zsh
@@ -24,9 +24,8 @@ bindkey "\C-m" alls
 compinit
 
 export LSCOLORS=ExFxCxDxBxEgedabagacad
-export LS_COLORS='di=34:ln=35:so=32:pi=33:ex=31:bd=46;34:cd=43;34:su=41;30:sg=46;30:tw=42;30:ow=43;30'
-
-zstyle ':completion:*' list-colors 'di=34' 'ln=35' 'so=32' 'ex=31' 'bd=46;34' 'cd=43;34'
+#export LS_COLORS='di=34:ln=35:so=32:pi=33:ex=31:bd=46;34:cd=43;34:su=41;30:sg=46;30:tw=42;30:ow=43;30'
 
 # 補完後、メニュー選択モードになり左右キーで移動が出来る
 zstyle ':completion:*:default' menu select=2
+zstyle ':completion:*' list-colors ${LSCOLORS}
