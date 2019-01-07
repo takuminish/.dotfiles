7ad474dace26b3a17b5a760c7d3bcad5ad880ab2
takuminv <takuminv@TakuAir.local>
2019-01-08 00:52:06 +0900

    branchのマークを追加

--- .zsh.d/rightprompt.zsh
+++ .zsh.d/rightprompt.zsh
@@ -2,9 +2,9 @@
 autoload -Uz vcs_info
 setopt prompt_subst
 zstyle ':vcs_info:git:*' check-for-changes true
-zstyle ':vcs_info:git:*' stagedstr "%F{yellow}\ue0a0 "
-zstyle ':vcs_info:git:*' unstagedstr "%F{red}\ue0a0 "
-zstyle ':vcs_info:*' formats "%F{green}%c%u[%b]%f"
+zstyle ':vcs_info:git:*' stagedstr "%F{yellow}"
+zstyle ':vcs_info:git:*' unstagedstr "%F{red}"
+zstyle ':vcs_info:*' formats "%F{green}%c%u\ue0a0 [%b]%f"
 zstyle ':vcs_info:*' actionformats '[%b|%a]'
 precmd () { vcs_info }
 
