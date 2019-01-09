a5e603592492974e4e894d839a43be15293df978
takuminish <s15t253@stu.kagawa-u.ac.jp>
2019-01-10 02:19:41 +0900

    branchの表示を変更

--- .zsh.d/leftprompt.zsh
+++ .zsh.d/leftprompt.zsh
@@ -31,7 +31,7 @@ setopt prompt_subst
 zstyle ':vcs_info:git:*' check-for-changes true
 zstyle ':vcs_info:git:*' stagedstr "%F{yellow}"
 zstyle ':vcs_info:git:*' unstagedstr "%F{red}"
-zstyle ':vcs_info:*' formats "%F{green}%c%u \ue0a0[%b]%f"
+zstyle ':vcs_info:*' formats "%F{green}%c%u \ue0a0[%b-branch]%f"
 zstyle ':vcs_info:*' actionformats '[%b|%a]'
 precmd () { vcs_info }
 
