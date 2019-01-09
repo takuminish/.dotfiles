fd0a5afe75ebdb8452777f1c43f616fd721e7774
takuminish <s15t253@stu.kagawa-u.ac.jp>
2019-01-09 21:49:05 +0900

    branch名のスペースを変更&rightpromptを削除

--- .zsh.d/leftprompt.zsh
+++ .zsh.d/leftprompt.zsh
@@ -27,7 +27,7 @@ setopt prompt_subst
 zstyle ':vcs_info:git:*' check-for-changes true
 zstyle ':vcs_info:git:*' stagedstr "%F{yellow}"
 zstyle ':vcs_info:git:*' unstagedstr "%F{red}"
-zstyle ':vcs_info:*' formats "%F{green}%c%u\ue0a0[%b]%f"
+zstyle ':vcs_info:*' formats "%F{green}%c%u \ue0a0[%b]%f"
 zstyle ':vcs_info:*' actionformats '[%b|%a]'
 precmd () { vcs_info }
 
@@ -35,7 +35,7 @@ precmd () { vcs_info }
 
 function git-prompt {
     if [[ -z ${vcs_info_msg_0_} ]]; then
-	echo '%F{white}\ue0a0 [no-branch]%f'
+	echo '%F{white} \ue0a0 [no-branch]%f'
     else
         echo ${vcs_info_msg_0_}
     fi
