4ec649e6eda930d77f3b94ea80dc5ce73900df88
takuminish <s15t253@stu.kagawa-u.ac.jp>
2019-01-09 19:31:34 +0900

    ユーザ名とカレントパスの表示を変更

--- .zsh.d/leftprompt.zsh
+++ .zsh.d/leftprompt.zsh
@@ -5,10 +5,10 @@ FACE_COLOR=("03" "207" "156" "124" "19" "07" "214" "93" "05")
 # カレントディレクトリと顔文字を表示
 
 PROMPT='$(left-prompt)
-%(?.%B%F{green}.%B%F{blue})[%n] %(?!$FACE[$RANDOM % ${#FACE[@]} + 1] ! |c||^.- ^|| < ぶっぶーですわ )%f%b'
+%(?.%B%F{green}.%B%F{blue})%(?!$FACE[$RANDOM % ${#FACE[@]} + 1] ! |c||^.- ^|| < ぶっぶーですわ )%f%b'
 
 function left-prompt() {
-  echo '%K{cyan}%d%k%F{cyan}\ue0b0%f'
+  echo '%K{green}%F{white}[%n] %f%k%F{green}%K{cyan}\ue0b0%f %d %k%F{cyan}\ue0b0%f'
 }
  
 # エンターキー押すたびに顔文字を変化
