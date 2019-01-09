8ed6f4062f93b578fc15a4ba0d4a03e800cdabe6
takuminish <s15t253@stu.kagawa-u.ac.jp>
2019-01-09 21:43:04 +0900

    branch名を緑にする

--- .zsh.d/leftprompt.zsh
+++ .zsh.d/leftprompt.zsh
@@ -27,7 +27,7 @@ setopt prompt_subst
 zstyle ':vcs_info:git:*' check-for-changes true
 zstyle ':vcs_info:git:*' stagedstr "%F{yellow}"
 zstyle ':vcs_info:git:*' unstagedstr "%F{red}"
-zstyle ':vcs_info:*' formats "%c%u\ue0a0[%b]%f%k"
+zstyle ':vcs_info:*' formats "%F{green}%c%u\ue0a0[%b]%f"
 zstyle ':vcs_info:*' actionformats '[%b|%a]'
 precmd () { vcs_info }
 
