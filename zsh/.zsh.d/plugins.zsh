c0b2dd3ec3efa3547d767219078dc3e450e0abfe
takuminish <s15t253@stu.kagawa-u.ac.jp>
2019-01-10 18:18:54 +0900

    コマンドにハイライトをつけた

--- zsh/.zsh.d/plugins.zsh
+++ zsh/.zsh.d/plugins.zsh
@@ -2,6 +2,7 @@ export ZPLUG_HOME=/usr/local/opt/zplug
 source $ZPLUG_HOME/init.zsh
 
 zplug "zsh-users/zsh-autosuggestions", defer:2  
+zplug "zsh-users/zsh-syntax-highlighting", defer:2
 
 # Install plugins if there are plugins that have not been installed
 if ! zplug check --verbose; then
