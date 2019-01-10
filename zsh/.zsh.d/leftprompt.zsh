e404326149837fcd730965454ae1537c0a9e63f5
takuminish <s15t253@stu.kagawa-u.ac.jp>
2019-01-10 14:13:45 +0900

    スペースを追加

--- zsh/.zsh.d/leftprompt.zsh
+++ zsh/.zsh.d/leftprompt.zsh
@@ -9,18 +9,19 @@ $(left-prompt-middle)
 %(?.%B%F{green}.%B%F{blue})%(?!$FACE[$RANDOM % ${#FACE[@]} + 1] !|c||^.- ^|| < ぶっぶーですわ )%f%b'
 
 function left-prompt-top() {
-  echo "%K{magenta}$(ifconfig | grep -w "inet" | grep -v "127.0.0.1" | cut -f 2 -d ' ')%k%K{green}%F{magenta}\ue0b0%f %F{black}[%n] %f%k%F{green}%K{cyan}\ue0b0%f %F{black}%d %f%k%F{cyan}%K{black}\ue0b0%f $(git-prompt)%k%F{black}\ue0b0%f"
+  echo "%K{magenta}$(ifconfig | grep -w "inet" | grep -v "127.0.0.1" | cut -f 2 -d ' ') %k%K{green}%F{magenta}\ue0b0%f %F{black}[%n] %f%k%F{green}%K{cyan}\ue0b0%f %F{black}%d %f%k%F{cyan}%K{black}\ue0b0%f $(git-prompt)%k%F{black}\ue0b0%f"
 }
 
 function left-prompt-middle() {
-  echo "%K{white}%F{black}$(echo $SHELL | cut -f 5 -d /)%f%k%F{white}%K{red}\ue0b0%f%F{black}  Ruby $(rbenv version | cut -f 1 -d ' ')%f%k%F{red}%K{blue}\ue0b0%f  gcc $(gcc -dumpversion)%k%F{blue}\ue0b0%f"
+  echo "%K{white}%F{black}$(echo $SHELL | cut -f 5 -d /) %f%k%F{white}%K{red}\ue0b0%f%F{black}  Ruby $(rbenv version | cut -f 1 -d ' ') %f%k%F{red}%K{blue}\ue0b0%f  gcc $(gcc -dumpversion) %k%F{blue}\ue0b0%f"
 }
 # エンターキー押すたびに顔文字を変化
 alls() {
   zle accept-line
   if [[ -z "$BUFFER" ]]; then
       echo ''
-      PROMPT='$(left-prompt)
+      PROMPT='$(left-prompt-top)
+$(left-prompt-middle)
 %(?.%B%F{green}.%B%F{blue})%(?!$FACE[$RANDOM % ${#FACE[@]} + 1] !|c||^.- ^|| < ぶっぶーですわ )%f%b'
   fi
 }
