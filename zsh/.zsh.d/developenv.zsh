a98e9d43fdb2082bc688b9f7616f541ad9e22532
takuminish <s15t253@stu.kagawa-u.ac.jp>
2019-01-16 02:08:06 +0900

    pyenvを削除

--- zsh/.zsh.d/developenv.zsh
+++ zsh/.zsh.d/developenv.zsh
@@ -1,11 +1,4 @@
 # rbenvのパス
 export PATH="$HOME/.rbenv/bin:$PATH"
 eval "$(rbenv init -)"
-# openssl
-export PATH=$PATH:"/opt/local/bin:/opt/local/sbin"
-export LIBRARY_PATH=$LIBRARY_PATH:"/usr/local/opt/openssl/lib/"
 
-# pyenv
-export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init -)"
