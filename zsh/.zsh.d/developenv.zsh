87c6fbab840574bd4f54946e00c93249c1d0575c
takuminish <s15t253@stu.kagawa-u.ac.jp>
2019-09-11 21:53:08 +0900

    zplugがないときはgit clone

--- zsh/.zsh.d/developenv.zsh
+++ zsh/.zsh.d/developenv.zsh
@@ -1,4 +1,6 @@
 # rbenvのパス
 export PATH="$HOME/.rbenv/bin:$PATH"
 eval "$(rbenv init -)"
-
+# openssl
+export PATH=$PATH:"/opt/local/bin:/opt/local/sbin"
+export LIBRARY_PATH=$LIBRARY_PATH:"/usr/local/opt/openssl/lib/"
--- zsh/.zsh.d/plugins.zsh
+++ zsh/.zsh.d/plugins.zsh
@@ -1,16 +1,20 @@
-export ZPLUG_HOME=/usr/local/opt/zplug
-source $ZPLUG_HOME/init.zsh
+
+source ~/.zplug/init.zsh
 
 zplug "zsh-users/zsh-autosuggestions", defer:2  
 zplug "zsh-users/zsh-syntax-highlighting", defer:2
 
 # Install plugins if there are plugins that have not been installed
-if ! zplug check --verbose; then
-    printf "Install? [y/N]: "
-    if read -q; then
-        echo; zplug install
+if [ -e "${HOME}/.zplug" ]; then
+    if ! zplug check --verbose; then
+        printf "Install? [y/N]: "
+        if read -q; then
+            echo; zplug install
+        fi
     fi
+else
+    git clone https://github.com/b4b4r07/zplug ~/.zplug
 fi
 
 # Then, source plugins and add commands to $PATH
-zplug load --verbose
+zplug load
